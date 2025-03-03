9ff43e166c17d5f132bcf924c2b1a7651248d4a8
lucasrnramos <lucas.rodrigues@unimedmaceio.com.br>
2025-03-03 16:31:45 -0300

    Ajuste na rota de login

--- technology-solutions-api/app/Http/Controllers/Convite/ConviteController.php
+++ technology-solutions-api/app/Http/Controllers/Convite/ConviteController.php
@@ -77,7 +77,7 @@ class ConviteController extends Controller
             // Envia o e-mail
             $disparo = Mail::to($request->email)->send(new ConviteMail($request->email, $hash));
 
-            // Salva os dados na tabela convites (por algum motivo a model não estava sendo localizada)
+            // Salva os dados na tabela convites (por algum motivo a model não estava sendo localizada, então utilizei a classe DB abaixo)
             /*
             $convite = new Convite();
 
