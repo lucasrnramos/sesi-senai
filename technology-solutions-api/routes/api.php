b34cd81b10e9c1bf7d158b732c2b2aa754514927
lucasrnramos <lucas.rodrigues@unimedmaceio.com.br>
2025-03-03 18:13:19 -0300

    Criação de rota para redefinição de senha

--- technology-solutions-api/routes/api.php
+++ technology-solutions-api/routes/api.php
@@ -21,4 +21,11 @@ Route::get('/perfis', [PerfisController::class, 'index']);
 Route::post('/perfis/criar', [PerfisController::class, 'store']);
 Route::patch('/perfis/editar/{cpf}/{id_perfil}', [PerfisController::class, 'update']);
 Route::post('/convite/criar', [ConviteController::class, 'store']);
+
+//Rota que envia um convite para redefinir senha;
+Route::get('/convite/redefinir/{email}', [ConviteController::class, 'storePassword']);
+
 Route::get('/convite', [ConviteController::class, 'index']);
+
+// Rota para redefinir senha;
+Route::post('/login/redefinir', [LoginController::class, 'update']);
